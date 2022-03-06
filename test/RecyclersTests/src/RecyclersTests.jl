05ca81341e62e724ab08dc93a37f45d9307045f1
Takafumi Arakaki <aka.tkf@gmail.com>
2022-03-06 01:09:22 -0800

    Setup Aqua (#2)

--- test/RecyclersTests/src/RecyclersTests.jl
+++ test/RecyclersTests/src/RecyclersTests.jl
@@ -3,5 +3,6 @@ module RecyclersTests
 include("utils.jl")
 include("test_recyclers.jl")
 include("test_global.jl")
+include("test_aqua.jl")
 
 end  # module RecyclersTests
---
+++ test/RecyclersTests/src/test_aqua.jl
@@ -0,0 +1,8 @@
+module TestAqua
+
+using Recyclers
+using Aqua
+
+test() = Aqua.test_all(Recyclers)
+
+end  # module
